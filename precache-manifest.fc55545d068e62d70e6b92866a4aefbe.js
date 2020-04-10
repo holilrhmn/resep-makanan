5146894fd16592946f4b2e59c00c2420dcceaa8e
holilrhmn <holilrahman10@gmail.com>
2020-04-10 10:21:01 +0800

    Update Terbaru

---
+++ precache-manifest.fc55545d068e62d70e6b92866a4aefbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f052b7ceb25abe5378edcdad0f45534d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecb0d30e6eca426467c",
+    "url": "/static/css/main.db39fdc7.chunk.css"
+  },
+  {
+    "revision": "5fa93ffb570fc34a1a46",
+    "url": "/static/js/2.ab45934a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab45934a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecb0d30e6eca426467c",
+    "url": "/static/js/main.109eb29e.chunk.js"
+  },
+  {
+    "revision": "e81394b9fbfb5adb6e4a",
+    "url": "/static/js/runtime-main.31890634.js"
+  },
+  {
+    "revision": "1b2c465fca4f8fcb1fd0e7c795c640dc",
+    "url": "/static/media/banner-01.1b2c465f.jpg"
+  }
+]);
